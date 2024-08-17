303c2dac6019c19d4afa467417581245c42f5eec
hayabusa1601 <1601hayabusa@gmail.com>
2024-08-17 21:06:39 +0900

    [feat] メタデータの入力機能を実装

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import os
 from pydub import AudioSegment # ffmpegが必要
 from mutagen.flac import FLAC
 
+
 def main():
   # 入力
   #wav_file = input("wavファイルの読み込み:")
-  wav_folder_path = "test/"
+  wav_folder_path = input("folder path?:")
+  
+  # メタデータの入力
+  album_artist_name = input("album artist name?:")
+  genre_name = input("genre name?: ")
 
-  track_count = 1
+
+  # 処理
   for filename in os.listdir(wav_folder_path):
     if filename.lower().endswith('.wav'):
       input_file = os.path.join(wav_folder_path, filename)
-      output_file = wav_to_flac(input_file)
-      print("track" + str(track_count) + ": " + f"{output_file}")
+      output_file = wav_to_flac(input_file, album_artist_name, genre_name)
+      print(f"{output_file}")
       print("Converted")
-      track_count += 1
       
 
   print("Success")
@@ -22,7 +27,7 @@ def main():
 
 
 # wavからflac
-def wav_to_flac(wav_path):
+def wav_to_flac(wav_path, album_artist_name, genre_name):
 
   # === ファイルの前処理 ===
   # フォルダ名とファイル名に分割
@@ -50,12 +55,22 @@ def wav_to_flac(wav_path):
 
   # flacに変換
   audio.export(output_file, format='flac')
-
+  
+  print("filename:" + file_name)
+  
+  
+  artist_name = input("artist?:")
+  track_num= input("tracknumer?: ")
 
   # メタデータの追加
   flac_audio = FLAC(output_file)
   flac_audio["TITLE"] = file_name
   flac_audio["ALBUM"] = album_name
+  flac_audio["ARTIST"] = artist_name
+  flac_audio["ALBUMARTIST"] = album_artist_name
+  flac_audio["TRACKNUMBER"] = track_num
+  flac_audio["genre"] = genre_name
+
   flac_audio.save()
 
   return output_file
