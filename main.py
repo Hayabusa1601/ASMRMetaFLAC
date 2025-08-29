92b31bfe71b35f076d5f24a8485b82bffe2bab39
hayabusa1601 <1601hayabusa@gmail.com>
2025-08-29 19:29:06 +0900

    [feat] メタデータ入力をGUIに変更

--- main.py
+++ main.py
@@ -5,10 +5,47 @@ import time
 import tkinter as tk
 import tkinter.filedialog
 
-def btn_apply():
-  id = txt_id.get()
+def btn_apply(txt_id, txt_artist_name, txt_album_artist, txt_genre_name,root, wav_folder_path):
   
+  # tkinterオブジェクトからテキストを取得
+  id = txt_id.get()
+  artist_name = txt_artist_name.get()
+  album_artist_name = txt_album_artist.get()
+  genre_name = txt_genre_name.get()
 
+  # idは最低限必要なので入力必須
+  if not id:
+    tkinter.messagebox.showerror("エラー", "IDが入力されていません")
+    return
+  
+  root.destroy() # GUIウィンドウを一旦非表示
+
+  print(f"ID:{id}の処理を開始...")
+  #try:
+  print("アートワークを取得中・・・")
+  # アートワークの入手
+  artwork_filepath = dlsite_data_collector(id)
+  print(f"アートワークを保存しました。path:{artwork_filepath}")
+
+  # ファイル返還処理
+  found_wav_files = False
+  for filename in os.listdir(wav_folder_path):
+    if filename.lower().endswith('.wav'):
+      found_wav_files = True
+      print(f"\n変換中...:{filename}")
+      input_file = os.path.join(wav_folder_path, filename)
+      output_file = wav_to_flac(input_file,artist_name, album_artist_name, genre_name,artwork_filepath)
+      print(f"\n返還完了: {output_file}")
+
+  if not found_wav_files:
+    print("指定されたフォルダにwavファイルが見つかりませんでした")
+    
+  print("\nすべての処理が終了しました")
+  #except Exception as e:
+  #  print(f"エラーが発生しました：{e}")
+  #  tkinter.messagebox.showerror("エラー", f"処理中にエラーが発生しました：\n{e}")
+
+    
 
 def main():
   
@@ -17,49 +54,55 @@ def main():
   print("====dlsite2flac====")
   print("wavファイルが格納されているフォルダを選択してください。")
   time.sleep(1)
-  wav_folder_path = tkinter.filedialog.askdirectory()
 
   # ウィンドウの表示
   root = tk.Tk()
-  root.geometry("500x500")
+  root.withdraw()
+  wav_folder_path = tkinter.filedialog.askdirectory()
+  if not wav_folder_path:
+    print("フォルダが選択されませんでした")
+    return
+  
+  root.deiconify()
+  root.geometry("400x300")
   # 画面タイトル　
   root.title("dlsite2flac")
   # idの入力
-  lbl_id = tk.Label(text="id (RJを含む)")
+  lbl_id = tk.Label(text="作品id (例：RJ123456)")
   lbl_id.place(x=30, y=70)
 
+  txt_id = tkinter.Entry(width=20) # id入力欄の作成
+  txt_id.place(x=180,y=70)
 
-  txt_id = tkinter.Entry(width=20)
-  txt_id.place(x=150,y=70)
+  # アーティスト名の入力
+  lbl_artist_name = tk.Label(text="アーティスト名")
+  lbl_artist_name.place(x=30, y=100)
 
-  btn = tkinter.Button(root, text="apply", commant=btn_apply)
-  btn.place(x=140, y=170)
+  txt_artist_name = tkinter.Entry(width=20)
+  txt_artist_name.place(x=180, y=100)
+
+  # アルバムアーティスト名の入力
+  lbl_album_artist = tk.Label(text="アルバムアーティスト名")
+  lbl_album_artist.place(x=30, y=130)
+  
+  txt_album_artist = tkinter.Entry(width=20)
+  txt_album_artist.place(x=180, y=130)
+
+  # ジャンル名の入力
+  lbl_genre_name = tk.Label(text="ジャンル名")
+  lbl_genre_name.place(x=30, y=160)
+
+  txt_genre_name = tkinter.Entry(width=20)
+  txt_genre_name.place(x=180, y=160)
+
+
+  btn = tkinter.Button(root, 
+                       text="apply", 
+                       command=lambda: btn_apply(txt_id,txt_artist_name,txt_album_artist,txt_genre_name, root,wav_folder_path))
+  btn.place(x=140, y=200)
 
   #表示
   root.mainloop()
-
-
-  # アートワークの入手
-  artwork_filepath = dlsite_data_collector(txt_id.get)
-
-
-  # メタデータの入力
-  artist_name = input("artist name?: ")
-  album_artist_name = input("album artist name?:")
-  genre_name = input("genre name?: ")
-
-
-  # 処理
-  for filename in os.listdir(wav_folder_path):
-    if filename.lower().endswith('.wav'):
-      print("Converting...")
-      input_file = os.path.join(wav_folder_path, filename)
-      output_file = wav_to_flac(input_file,artist_name, album_artist_name, genre_name,artwork_filepath)
-      print(f"{output_file}")
-      print("Converted")
-      
-
-  print("Success")
 
 
 if __name__ == "__main__":
